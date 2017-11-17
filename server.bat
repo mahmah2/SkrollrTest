a4ccb0527978a117a81d7babda92fe7f712791d0
mahmah2 <mahfoozi@gmail.com>
2017-11-17 15:50:03 +1100

    Added effect to last three divs

--- server.bat
+++ server.bat
@@ -1,3 +1,11 @@
+@echo off
+echo  __      __        _        ___                               
+echo  \ \    / /  ___  ^| ^|__    / __^|  ___   _ _  __ __  ___   _ _ 
+echo   \ \/\/ /  / -_) ^| '_ \   \__ \ / -_) ^| '_^| \ V / / -_) ^| '_^|
+echo    \_/\_/   \___^| ^|_.__/   ^|___/ \___^| ^|_^|    \_/  \___^| ^|_^|  
+echo                                                               
+echo 
+
 rem npm install http-server -g
 
 rem start http://localhost:8080
